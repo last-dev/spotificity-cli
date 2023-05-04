7cabdb548ca0c8d6bc6d99bb62a467d50ed4133b
Jonathan <jonathan@laster.dev>
2023-05-04 07:00:25 -0400

    Clean up menu flow-control

--- cli/main.py
+++ cli/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-from ui.colors import Colors, print_colors, colorfy
+from ui.colors import Colors, colorfy
 from actions.actions import (
     list_artists,
     add_artist,
     remove_artist,
     request_token,
+    get_valid_user_input,
     quit
 )
 import subprocess
@@ -22,32 +23,32 @@ def title() -> None:
 \033[0m""")
 
 
-def main_menu() -> tuple[int, dict]:
+def main_menu() -> tuple[str, dict]:
     """
     Main menu where user can select what actions they want to take
     """
     
     title()
     menu_choices = {
-        1: {
+        '1': {
             "choice_name": f"\n\t[{colorfy(Colors.GREEN, '1')}] List Out Current Monitored Artists", 
             "function": list_artists,
             "token_needed": False,  # Indicates function requires access token to fetch data from Spotify API
             "continue_prompt": True  # If called from main menu, loop back to menu when done
             },
-        2: {
+        '2': {
             "choice_name": f"\n\t[{colorfy(Colors.GREEN, '2')}] Add New Artist to List", 
             "function": add_artist,
             "token_needed": True,
             "continue_prompt": True
             },
-        3: {
+        '3': {
             "choice_name": f"\n\t[{colorfy(Colors.GREEN, '3')}] Remove Artist From List", 
             "function": remove_artist,
             "token_needed": False,
             "continue_prompt": True
             },
-        4: {
+        '4': {
             "choice_name": f"\n\t[{colorfy(Colors.GREEN, '4')}] Quit App", 
             "function": quit,
             "token_needed": False,
@@ -59,20 +60,17 @@ def main_menu() -> tuple[int, dict]:
     print("\t\t===========")
 
     # List out menu choices
-    for choices in menu_choices:
-        print(menu_choices[choices]["choice_name"])
+    for choice in menu_choices:
+        print(menu_choices[choice]["choice_name"])
     
     # Fetch user choice. Check to make sure it is a proper selection
-    while True:
-        try:
-            user_choice = int((input("\nWhat would you like to do? Make a selection:\n> ")))
-            if user_choice in list(range(1, len(menu_choices.keys()) + 1)):
-                return user_choice, menu_choices
-            else:
-                raise ValueError
-        except ValueError:
-            print_colors(Colors.YELLOW, "\n\tPlease enter a valid selection.")
+    valid_choices: list[str] = [menu_choice_key for menu_choice_key in menu_choices.keys()]
+    user_choice = get_valid_user_input(
+        prompt='\nWhat would you like to do? Make a selection:\n> ',
+        valid_choices=valid_choices
+    )
 
+    return user_choice, menu_choices
 
 def main() -> None:
     
