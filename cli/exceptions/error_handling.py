a6dc815ae01dd6b9847a493fb5785c6ee21cf2f0
Jonathan <jonathan@laster.dev>
2023-05-04 03:43:55 -0400

    Add 2 new custom exception classes

--- cli/exceptions/error_handling.py
+++ cli/exceptions/error_handling.py
@@ -19,7 +19,7 @@ class FailedToRetrieveMonitoredArtists(Exception):
     
 class ExceptionDuringLambdaExecution(Exception):
     """
-    Raised when an exception is raised during execution of a Lambda function
+    When an exception is raised during execution of a Lambda function, this exception is raised here at the client
     """
     def __init__(self, lambda_name: str, error_message: str) -> None:
         self.error_message = error_message
@@ -30,10 +30,30 @@ class ExceptionDuringLambdaExecution(Exception):
         
 class FailedToRetrieveListOfMatchesWithIDs(Exception):
     """
-    Raised when scan operation against DynamoDB table fails
+    Raised when a GET request to the Spotify API fails
     """
     def __init__(self, error_message: str) -> None:
         self.error_message = error_message
 
     def __str__(self) -> str:
         return colorfy(Colors.RED, f'\nFailed to retrieve list of close matches to user choice from Spotify API: \n\n{self.error_message}')
+    
+class FailedToAddArtistToTable(Exception):
+    """
+    Raised when PUT operation on DynamoDB table fails
+    """
+    def __init__(self, error_message: str) -> None:
+        self.error_message = error_message
+
+    def __str__(self) -> str:
+        return colorfy(Colors.RED, f'\nFailed to add artist to DynamoDB table: \n\n{self.error_message}')
+    
+class FailedToRemoveArtistFromTable(Exception):
+    """
+    Raised when DELETE operation on DynamoDB table fails
+    """
+    def __init__(self, error_message: str) -> None:
+        self.error_message = error_message
+
+    def __str__(self) -> str:
+        return colorfy(Colors.RED, f'\nFailed to remove artist from DynamoDB table: \n\n{self.error_message}')
