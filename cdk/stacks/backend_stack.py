528e3ca461bf456ecd1df67ebd4d073e9ee2b13c
Jonathan <jonathan@laster.dev>
2023-05-06 04:39:17 -0400

    Update new Stack names

--- cdk/stacks/backend_stack.py
+++ cdk/stacks/backend_stack.py
@@ -1,10 +1,9 @@
-from stacks.custom_constructs.table_manipulators import TableManipulatorsConstruct
-from stacks.custom_constructs.spotify_operators import SpotifyOperatorsConstruct
+from stacks.custom_constructs.table_operators import CoreTableOperatorsConstruct
+from stacks.custom_constructs.spotify_operators import CoreSpotifyOperatorsConstruct
 from constructs import Construct
 from aws_cdk import (
     Stack,
-    aws_dynamodb as ddb,
-    Fn
+    aws_dynamodb as ddb
 )
 
 class BackendStack(Stack):
@@ -13,15 +12,15 @@ class BackendStack(Stack):
         
         # Custom construct with setter, getter, and deleter Lambda functions 
         # for manipulating 'Monitored Artists' DynamoDB table
-        table_manipulators = TableManipulatorsConstruct(
+        table_operators = CoreTableOperatorsConstruct(
             self, 'TableManipulatorsConstruct',
             artist_table=monitored_artist_table
         )
         
         # Custom construct for the resources that will interact with the Spotify API
-        spotify_operators = SpotifyOperatorsConstruct(
+        spotify_operators = CoreSpotifyOperatorsConstruct(
             self, 'SpotifyOperatorsConstruct',
             artist_table_arn=monitored_artist_table.table_arn,
             artist_table_stream_arn=monitored_artist_table.table_stream_arn,  # type: ignore
-            update_table_music_lambda=table_manipulators.update_table_music_lambda
+            update_table_music_lambda=table_operators.update_table_music_lambda
         )
