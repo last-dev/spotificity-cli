6ee102fe4f964ee70e8065ecf6b59efd291e6b31
Jonathan <jonathan@laster.dev>
2023-05-07 06:49:58 -0400

    Create handler file for artist list fetcher (for step function)

---
+++ cdk/lambda_functions/GetArtistListForNotifierHandler/get_artist_list_for_notifier.py
@@ -0,0 +1,10 @@
+from botocore.exceptions import ClientError
+import boto3
+import os
+
+def handler(event: dict, context) -> dict:
+    """
+    Returns a list of all current artists being monitored. 
+    """
+
+    pass
