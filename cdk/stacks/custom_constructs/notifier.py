bd5a6b1c021901247634372811100dc261e3f692
Jonathan <jonathan@laster.dev>
2023-05-12 05:11:59 -0400

    Import my phone numbers for the SNS topic via Secrets Manager

--- cdk/stacks/custom_constructs/notifier.py
+++ cdk/stacks/custom_constructs/notifier.py
@@ -11,12 +11,21 @@ from aws_cdk import (
     aws_events_targets as target,
     aws_dynamodb as ddb,
     aws_stepfunctions as stepfunction,
-    aws_stepfunctions_tasks as task
+    aws_stepfunctions_tasks as task,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    aws_secretsmanager as ssm
 )
 
 class NotifierConstruct(Construct):
     def __init__(self, scope: Construct, id: str, artist_table: ddb.Table, requests_layer: lambda_.LayerVersion, access_token_lambda: lambda_.Function, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+        
+        # Import my mobile numbers to add as subscriptions to the SNS topic
+        __my_numbers = ssm.Secret.from_secret_name_v2(
+            self, 'ImportedMobileNumbers',
+            secret_name='PhoneNumberSecrets'
+        )
    
         # Lambda to pull the current list of artists being monitored
         _fetch_artists_lambda = lambda_.Function(
@@ -58,7 +67,7 @@ class NotifierConstruct(Construct):
 
         # TODO: If _scan_task returns with a status code of 204, we immediately publish SNS message to the topic.
         # Otherwise, we invoke the lambda to fetch the latest music released all artists 
-        _choice_state = stepfunction.Choice(self, 'Artists or not...')
+        _choice_state = stepfunction.Choice(self, 'TaskSplit')
 
         """ Define states for choice state """
         _fetch_latest_music_task = task.LambdaInvoke(
@@ -66,8 +75,23 @@ class NotifierConstruct(Construct):
             lambda_function=_fetch_music_lambda,  # type: ignore
             payload_response_only=True
         )
+        
+        _topic = sns.Topic(
+            self, 'NotifierTopic',
+            topic_name='SpotificityNotifierTopic'
+        )  
+        
+        # Add my phone number as subscription to the topic.
+        cell_number = __my_numbers.secret_value_from_json('MOBILE_NUMBER').to_string
+        google_voice_number = __my_numbers.secret_value_from_json('GOOGLE_VOICE_NUMBER').to_string
+        _topic.add_subscription(subs.SmsSubscription(cell_number))
+        _topic.add_subscription(subs.SmsSubscription(google_voice_number))
 
-        _no_artists_publish_task = task.SnsPublish()
+        _no_artists_publish_task = task.SnsPublish(
+            self, 'PublishNoArtistsFound',
+            message=stepfunction.TaskInput.from_text('No artists found to monitor. Please add some artists to the table.'),
+            topic=_topic  # type: ignore
+        )
 
         """ Add conditions to the choice state """
         _choice_state.when(stepfunction.Condition.number_equals('$.payload.status_code', 204), _no_artists_publish_task)
@@ -95,3 +119,7 @@ class NotifierConstruct(Construct):
 
         # Grant the Lambda function to read from the Artist table
         artist_table.grant_read_data(_fetch_artists_lambda)
+        
+        # Grant my state machine permissions to publish to the Topic
+        _topic.grant_publish(_state_machine)
+        
