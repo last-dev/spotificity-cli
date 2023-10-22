03edb2171c38df85b08d94b876c34ec28ade98e3
Jonathan <jonathan@laster.dev>
2023-10-22 13:30:37 -0400

    Refactor lambda so that all return paths match the expected format for a Lambda proxy integration

--- cdk/lambda_functions/CoreSpotifyOperatorLambdas/get_artist_id.py
+++ cdk/lambda_functions/CoreSpotifyOperatorLambdas/get_artist_id.py
@@ -1,14 +1,16 @@
 from requests.exceptions import HTTPError
 import requests
+import json
 
 def handler(event: dict, context) -> dict:
     """
     Queries the Spotify `Search` API for the artist's Spotify ID. 
     """
     
-    print(f'Passed in artist payload: {event}')
-    artist_name: str = event['artist_name']
-    access_token: str = event['access_token']
+    print(f'Passed in artist payload: {event["body"]}')
+    payload: dict = json.loads(event['body'])
+    artist_name: str = payload['artist_name']
+    access_token: str = payload['access_token']
     endpoint: str = 'https://api.spotify.com/v1/search'
     
     try:
@@ -32,30 +34,42 @@ def handler(event: dict, context) -> dict:
         response.raise_for_status()
     except HTTPError as err:
         print(f'HTTP Error occurred: {err}')
-        raise
+        print(f'Unsuccessful retrieval from Spotify `Search` API. Returning error to client.')
+        return {
+            'statusCode': err.response.status_code,
+            'headers': {
+                'Content-Type': 'application/json'
+            },                
+            'body': json.dumps({
+                'error': err.response.text,
+                'error_type': 'HTTP'
+            })
+        }
     except Exception as err:
         print(f'Other error occurred: {err}')
-        raise
+        return {
+            'statusCode': 405,
+            'headers': {
+                'Content-Type': 'application/json'
+            },                
+            'body': json.dumps({
+                'error': str(err),
+                'error_type': 'Other'
+            })
+        }
     else:
         print(f'Successfully received response from Spotify `Search` API. HTTP Status code: {response.status_code}')
         print(f'Returned Payload: {response.json()}')
         artist_search_results: dict = response.json()
         
-        # Check if error occurred while attempting to fetch list of artists with their details
-        if artist_search_results.get('error'):
-            print(f'Unsuccessful retrieval from Spotify `Search` API. Returning error to client.')
-            return {
-                'statusCode': artist_search_results['error']['status'],
-                'payload': {
-                    'error': artist_search_results['error']
-                }
-            }
-        else:
-            print('Successfully retrieved list of artists with their respective Spotify IDs. Returning list to client.')
-            return {
-                'statusCode': 200,
-                'payload': {
-                    'artistSearchResultsList': artist_search_results['artists']['items']
-                }
-            }
+        print('Successfully retrieved list of artists with their respective Spotify IDs. Returning list to client.')
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({
+                'artistSearchResultsList': artist_search_results['artists']['items']
+            })
+        }
     
