b04390ccf82981c4cc0d3950213377ee1946ba81
Jonathan <jonathan@laster.dev>
2024-03-26 05:21:59 -0400

    Refactor: Upgrade Dynamo table to new V2 construct and move to individual construct class imports for readability

--- src/cdk/stacks/database_stack.py
+++ src/cdk/stacks/database_stack.py
@@ -1,35 +1,26 @@
+from aws_cdk import RemovalPolicy, Stack
+from aws_cdk.aws_dynamodb import AttributeType, StreamViewType, TableEncryptionV2, TableV2
 from constructs import Construct
-from aws_cdk import (
-    Stack,
-    RemovalPolicy,
-    aws_dynamodb as ddb,
-)
 
-class DatabaseStack(Stack):    
-    
+
+class DatabaseStack(Stack):
+
     @property
-    def artist_table(self) -> ddb.Table:
-        """ Returns the DynamoDB table name that holds the monitored artists. """
-        return self._artist_table
-    
+    def artist_table(self) -> TableV2:
+        """Returns the DynamoDB table name that holds the monitored artists."""
+        return self.table
+
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        
-        # DynamoDB table that stores all monitored artists
-        self._artist_table = ddb.Table(
-            self, 'MonitoredArtists', 
-            partition_key={
-                'name': 'artist_id',
-                'type': ddb.AttributeType.STRING
-            },
-            stream=ddb.StreamViewType.NEW_IMAGE,  # Enable stream for lambda invocations
-            encryption=ddb.TableEncryption.AWS_MANAGED,
-            removal_policy=RemovalPolicy.RETAIN
+
+        artist_table_name = 'MonitoredArtistsTable'
+        self.table = TableV2(
+            self,
+            artist_table_name,
+            partition_key={'name': 'artist_id', 'type': AttributeType.STRING},
+            encryption=TableEncryptionV2.aws_managed_key(),
+            removal_policy=RemovalPolicy.RETAIN,
+            dynamo_stream=StreamViewType.NEW_IMAGE,
+            table_name=artist_table_name,
+            point_in_time_recovery=True,
         )
-
-
-
-        
-
-        
-
