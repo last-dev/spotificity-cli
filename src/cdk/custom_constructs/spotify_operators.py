02ec632fe5c63d0b744122751d2bdf37ccef4a89
Jonathan <jonathan@laster.dev>
2024-03-26 10:33:17 -0400

    Refactor: Move to individual construct class imports for readability

--- src/cdk/custom_constructs/spotify_operators.py
+++ src/cdk/custom_constructs/spotify_operators.py
@@ -1,97 +1,95 @@
-"""
-Custom construct for core resources that will interact with
-the Spotify API. 
-"""
-
+from aws_cdk.aws_dynamodb import Table
+from aws_cdk.aws_lambda import Code, Function, LayerVersion, Runtime, StartingPosition
+from aws_cdk.aws_lambda_event_sources import DynamoEventSource
+from aws_cdk.aws_secretsmanager import Secret
 from constructs import Construct
-from aws_cdk import (
-    aws_lambda_event_sources as lambda_event_sources,
-    aws_secretsmanager as ssm, 
-    aws_lambda as lambda_,
-    aws_dynamodb as ddb
-)
+
 
 class CoreSpotifyOperatorsConstruct(Construct):
+    """
+    Custom construct for core resources that will interact with
+    the Spotify API.
+    """
 
     @property
-    def get_access_token_lambda(self) -> lambda_.Function:
-        return self._get_access_token_lambda
-    
-    @property
-    def get_artist_id_lambda(self) -> lambda_.Function:
-        return self._get_artist_id_lambda
+    def access_token_lambda(self) -> Function:
+        return self.get_access_token_lambda
 
-    def __init__(self, scope: Construct, id: str, 
-                 artist_table_arn: str, 
-                 artist_table_stream_arn: str, 
-                 update_table_music_lambda: lambda_.Function, 
-                 requests_layer: lambda_.LayerVersion, 
-                 **kwargs) -> None:
+    @property
+    def artist_id_lambda(self) -> Function:
+        return self.get_artist_id_lambda
+
+    def __init__(
+        self,
+        scope: Construct,
+        id: str,
+        artist_table_arn: str,
+        artist_table_stream_arn: str,
+        update_table_music_lambda: Function,
+        requests_layer: LayerVersion,
+        **kwargs,
+    ) -> None:
         super().__init__(scope, id, **kwargs)
-        
-        # Spotify access token 'getter' Lambda Function
-        self._get_access_token_lambda = lambda_.Function(
-            self, 'GetAccessToken',
-            runtime=lambda_.Runtime.PYTHON_3_10,
-            code=lambda_.Code.from_asset('lambdas/CoreSpotifyOperatorLambdas'),
+
+        get_access_token_lambda_name = 'GetAccessTokenLambda'
+        self.get_access_token_lambda = Function(
+            self,
+            get_access_token_lambda_name,
+            runtime=Runtime.PYTHON_3_10,
+            code=Code.from_asset('lambdas/CoreSpotifyOperatorLambdas'),
             handler='get_access_token.handler',
-            function_name='GetAccessTokenHandler',
-            description=f'Calls Spotify\'s "/token/" API endpoint to get an access token.',
-            layers=[requests_layer]
+            function_name=get_access_token_lambda_name,
+            description=f'Calls Spotify\'s API to get an access token.',
+            layers=[requests_layer],
         )
-        
-        # Spotify artist_id 'getter' Lambda Function
-        self._get_artist_id_lambda = lambda_.Function(
-            self, 'GetArtistID',
-            runtime=lambda_.Runtime.PYTHON_3_10,
-            code=lambda_.Code.from_asset('lambdas/CoreSpotifyOperatorLambdas'),
+
+        get_artist_id_lambda_name = 'GetArtist-IDLambda'
+        self.get_artist_id_lambda = Function(
+            self,
+            get_artist_id_lambda_name,
+            runtime=Runtime.PYTHON_3_10,
+            code=Code.from_asset('lambdas/CoreSpotifyOperatorLambdas'),
             handler='get_artist_id.handler',
-            function_name='GetArtist-IDHandler',
-            description=f'Queries the Spotify "/search" API endpoint for the artist ID.',
-            layers=[requests_layer]
+            function_name=get_artist_id_lambda_name,
+            description=f'Queries the Spotify\'s API for the artist\'s ID.',
+            layers=[requests_layer],
         )
-        
-        # Spotify artist's latest music 'getter' Lambda Function
-        _get_latest_music_lambda = lambda_.Function(
-            self, 'GetLatestMusic',
-            runtime=lambda_.Runtime.PYTHON_3_10,
-            code=lambda_.Code.from_asset('lambdas/CoreSpotifyOperatorLambdas'),
+
+        # Grant read permissions to my Spotify client secrets
+        __spotify_secrets = Secret.from_secret_name_v2(
+            self, 'ImportedSpotifySecrets', secret_name='SpotifySecrets'
+        )
+        __spotify_secrets.grant_read(self.get_access_token_lambda)
+
+        get_latest_music_lambda_name = 'GetLatestMusicLambda'
+        _get_latest_music_lambda = Function(
+            self,
+            get_latest_music_lambda_name,
+            runtime=Runtime.PYTHON_3_10,
+            code=Code.from_asset('lambdas/CoreSpotifyOperatorLambdas'),
             handler='get_latest_music.handler',
-            function_name='GetLatestMusicHandler',
-            description='Queries a series of Spotify API endpoints for the artist\'s latest music.',
+            function_name=get_latest_music_lambda_name,
+            description='Queries a series of Spotify API\'s for the artist\'s latest music.',
             layers=[requests_layer],
             environment={
-                'GET_ACCESS_TOKEN_LAMBDA': self._get_access_token_lambda.function_name,
-                'UPDATE_TABLE_MUSIC_LAMBDA': update_table_music_lambda.function_name
-            }
-        )     
-        
-        # Add DynamoDB Stream as an event source to trigger 'GetLatestMusicHandler'
-        # More info: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_lambda_event_sources/DynamoEventSource.html
-        _get_latest_music_lambda.add_event_source(
-            lambda_event_sources.DynamoEventSource(
-                table=ddb.Table.from_table_attributes(
-                    self, 'MonitoredArtistTable',
-                    table_arn=artist_table_arn,
-                    table_stream_arn=artist_table_stream_arn
-                ),
-                starting_position=lambda_.StartingPosition.LATEST,
-                retry_attempts=3,
-                bisect_batch_on_error=True, 
-            )
-        ) 
-        
-        # Import existent Spotify Secret to grant `GetAccessTokenHandler` Lambda permissions to pull secrets  
-        __spotify_secrets = ssm.Secret.from_secret_name_v2(
-            self, 'ImportedSpotifySecrets',
-            secret_name='SpotifySecrets'
+                'GET_ACCESS_TOKEN_LAMBDA': self.get_access_token_lambda.function_name,
+                'UPDATE_TABLE_MUSIC_LAMBDA': update_table_music_lambda.function_name,
+            },
         )
-        
-        # Give 'GetLatestMusicHandler' permission to invoke 'GetAccessTokenHandler'
-        self._get_access_token_lambda.grant_invoke(_get_latest_music_lambda)  
-        
-        # Give 'GetAccessTokenHandler' Lambda permissions to read secret
-        __spotify_secrets.grant_read(self._get_access_token_lambda)
-        
-        # Give 'LatestMusicGetter' Lambda permissions to invoke 'UpdateTableMusicLambda'
+        self.get_access_token_lambda.grant_invoke(_get_latest_music_lambda)
+
+        # Add DynamoDB Stream as an event source to trigger 'GetLatestMusicLambda'
+        _get_latest_music_lambda.add_event_source(
+            DynamoEventSource(
+                table=Table.from_table_attributes(
+                    self,
+                    'MonitoredArtistTable',
+                    table_arn=artist_table_arn,
+                    table_stream_arn=artist_table_stream_arn,
+                ),
+                starting_position=StartingPosition.LATEST,
+                retry_attempts=3,
+                bisect_batch_on_error=True,
+            )
+        )
         update_table_music_lambda.grant_invoke(_get_latest_music_lambda)
