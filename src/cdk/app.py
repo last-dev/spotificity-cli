6fdf59184c0f27575d18b1732ebafdee3fa7b082
Jonathan <jonathan@laster.dev>
2024-04-01 04:54:31 -0400

    Remove importing of env variables into Environment to rely on CLI arguments

--- src/cdk/app.py
+++ src/cdk/app.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 
-import os
-
-from aws_cdk import App, Environment
+from aws_cdk import App
 from stacks.backend_stack import BackendStack
 from stacks.database_stack import DatabaseStack
 
-default_account = Environment(
-    account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')
-)
 
 app = App()
-database_stack = DatabaseStack(app, 'DatabaseStack', env=default_account)
+
+database_stack = DatabaseStack(app, 'DatabaseStack')
 backend_stack = BackendStack(
     app,
     'BackendStack',
-    env=default_account,
     monitored_artist_table=database_stack.artist_table,
 )
 backend_stack.add_dependency(database_stack)
