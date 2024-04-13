da07bcf808d6db2258ef5494fd103ac6306ea3f5
Jonathan <jonathan@laster.dev>
2024-04-13 09:57:40 -0400

    Remove unused disambiguator and enum

--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -9,16 +9,11 @@ class Stage(Enum):
     Beta = 'Beta'
 
 
-class AwsRegion(Enum):
-    IAD = 'us-east-1'
-
-
 @dataclass(frozen=True)
 class Account:
     account_id: str
     stage: Stage
-    region: AwsRegion
-    disambiguator: str
+    region: str
     api_gw_endpoint_ssm_param_name: str
 
 
@@ -27,15 +22,13 @@ accounts: dict[str, Account] = {
     'Beta': Account(
         account_id=os.environ['SPOTIFICITY_BETA_ACCT'],
         stage=Stage.Beta,
-        region=AwsRegion.IAD,
-        disambiguator=f'spotificity',
+        region='us-east-1',
         api_gw_endpoint_ssm_param_name='/Spotificity/ApiGatewayEndpointUrl/beta',
     ),
     'Prod': Account(
         account_id=os.environ['SPOTIFICITY_PROD_ACCT'],
         stage=Stage.Prod,
-        region=AwsRegion.IAD,
-        disambiguator=f'spotificity',
+        region='us-east-1',
         api_gw_endpoint_ssm_param_name='/Spotificity/ApiGatewayEndpointUrl/prod',
     ),
 }
